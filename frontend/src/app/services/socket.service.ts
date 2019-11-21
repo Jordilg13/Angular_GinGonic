f3a88055a7800c777a0143536739f27c226dba90
Ethan Hildick <hildickethan@gmail.com>
2019-11-21 18:49:16 +0100

    changed sender id to int and added gin-gonic

--- frontend/src/app/services/socket.service.ts
+++ frontend/src/app/services/socket.service.ts
@@ -7,8 +7,8 @@ export class SocketService {
     private listener: EventEmitter<any> = new EventEmitter();
 
     public constructor() {
-        this.socket = new WebSocket("ws://192.168.27.44:12345/ws");
-        // this.socket = new WebSocket("ws://localhost:12345/ws");
+        // this.socket = new WebSocket("ws://192.168.27.44:12345/ws");
+        this.socket = new WebSocket("ws://localhost:12345/ws");
 
         this.socket.onopen = event => {
             this.listener.emit({"type": "open", "data": event});
