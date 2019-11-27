093678ded769b190f25b4bf246fa2fc1018a1f25
Ethan Hildick <hildickethan@gmail.com>
2019-11-27 16:53:02 +0100

    gorm creates character table

--- backend/go/common/db.go
+++ backend/go/common/db.go
@@ -7,14 +7,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/reji/backend/go/game"
 )
-
-// Name ...
-type Name struct {
-	gorm.Model
-	Name string
-	Test string
-}
 
 // Connection the gorm DB connection
 var Connection *gorm.DB
@@ -34,10 +28,11 @@ func ConnectSQL(host, port, uname, pass, dbname string) {
 	}
 
 	// Updates tables with the schema
-	db.AutoMigrate(&Name{})
+	db.AutoMigrate(&game.Character{})
 	Connection = db
 }
 
+// PrintDBResponse ...
 func PrintDBResponse(response *gorm.DB) {
 	jsonresponse, _ := json.Marshal(response)
 	fmt.Printf("%+v\n", string(jsonresponse))
